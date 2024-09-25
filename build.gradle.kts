e5c22fa8eec995cd2e5c1857dd489f991cca60f0
stephen seaton-blanchard <stephen@seatoncode.com>
2024-09-25 11:23:18 -0500

    Updates plugins Adds support for a default icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 
 
 group = "dev.kugge"
-version = "0.0.1"
+version = "0.0.2"
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
@@ -18,11 +18,12 @@ repositories {
     maven("https://jitpack.io")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.bluecolored.de/releases")
 }
 
 dependencies {
     compileOnly("dev.folia:folia-api:1.19.4-R0.1-SNAPSHOT")
-    compileOnly("com.github.BlueMap-Minecraft:BlueMapAPI:v2.4.0")
+    compileOnly ("de.bluecolored.bluemap:BlueMapAPI:2.7.2")
 }
 
 tasks.compileJava {
--- src/main/java/dev/kugge/signmarkers/watcher/SignWatcher.java
+++ src/main/java/dev/kugge/signmarkers/watcher/SignWatcher.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import com.flowpowered.math.vector.Vector3d;
 import com.flowpowered.math.vector.Vector2i;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -18,26 +19,30 @@ import java.io.IOException;
 public class SignWatcher implements Listener {
     @EventHandler
     public void onSignWrite(SignChangeEvent event) {
+        // Check for existing sign at position xyz and delete it
         Component header = event.line(0);
         if (header == Component.empty() || header == null) return;
         if (!header.toString().contains("[map]")) return;
 
+        // Don't require a line 3, in fact require a secondary tag and loop through for tags hell have defaults for [text] [poi] [map] etc.
         Component cicon = event.line(3);
         if (cicon == Component.empty() || cicon == null) return;
 
         String icon = "./markers/" + LegacyComponentSerializer.legacySection().serialize(cicon) + ".png";
         File iconFile = new File(SignMarkers.webRoot + "/" + icon);
-        if (!iconFile.exists()) return;
-
-        Vector2i anchor;
-        try {
-            BufferedImage image = ImageIO.read(iconFile);
-            int width = image.getWidth();
-            int height = image.getHeight();
-            anchor = new Vector2i(height/2, width/2);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
+        Vector2i anchor = new Vector2i(25, 45);
+        if (!iconFile.exists()) {
+            icon = "./assets/poi.svg";
+        } else {
+            try {
+                BufferedImage image = ImageIO.read(iconFile);
+                int width = image.getWidth();
+                int height = image.getHeight();
+                anchor = new Vector2i(height / 2, width / 2);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
         }
 
         Component clabel1 = event.line(1);
@@ -45,16 +50,16 @@ public class SignWatcher implements Listener {
 
         Component clabel2 = event.line(2);
         String label = LegacyComponentSerializer.legacySection().serialize(clabel1)
-                     + LegacyComponentSerializer.legacySection().serialize(clabel2);
+                + LegacyComponentSerializer.legacySection().serialize(clabel2);
 
         Block block = event.getBlock();
         Vector3d pos = new Vector3d(block.getX(), block.getY(), block.getZ());
 
         String id = "marker-" + pos.getX() + "-" + pos.getY() + "-" + pos.getZ();
-        POIMarker marker = POIMarker.builder().label(label).position(pos).icon(icon, anchor).maxDistance(100000).build();
+        POIMarker marker = new POIMarker(label, pos, icon, anchor);
         SignMarkers.markerSet.get(block.getWorld()).put(id, marker);
 
-        // Delete [map] and icon lines
+        // Leave [Map] and Icon in but don't add them to marker
         event.line(0, Component.empty());
         event.line(3, Component.empty());
     }
